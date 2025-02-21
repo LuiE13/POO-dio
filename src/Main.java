f3250444ac8528b64ca621761c273384b5b1c323
luiz <luies2901@gmail.com>
2025-02-20 22:26:13 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import br.com.dio.deasfio.dominio.Curso;
+import br.com.dio.deasfio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("'porra");
+        Curso curso1= new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2= new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("descricao curso javascript");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("Descrição mentoria de java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso2);
+        System.out.println(curso1);
+        System.out.println(mentoria);
     }
 }
